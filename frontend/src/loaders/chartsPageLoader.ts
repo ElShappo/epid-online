788578ce1b17107e47ce39c4ceecd9b92eb88d65
Deni Shaipov <deni.shaipp@gmail.com>
2023-10-29 12:41:52 +0300

    implemented delay for antd message

--- frontend/src/loaders/chartsPageLoader.ts
+++ frontend/src/loaders/chartsPageLoader.ts
@@ -1,3 +1,14 @@
+import {defer, redirect} from 'react-router-dom'
+import store from "../globalStore/store";
+
 export default function chartsPageLoader() {
+    const state = store.getState();
+    console.log('Got state in chartsPageLoader:');
+    console.log(state);
+
+    if (!state.authorization.authorized) {
+        console.warn('User is not authorized: redirecting...');
+        return redirect('/authorization');
+    }
     return '';
 }
--- frontend/src/pages/Charts/ChartsPage.tsx
+++ frontend/src/pages/Charts/ChartsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import LineChart from "../../components/LineChart";
 import { TreeSelect, message } from "antd";
 import "./ChartsPage.css";
@@ -7,37 +7,48 @@ const ChartsPage = () => {
   const [populationByAge, setPopulationByAge] = useState<string[]>();
   const [subjects, setSubjects] = useState<any>();
   const [messageApi] = message.useMessage();
-  const notificationKey = "updatable";
+  const loadingMessageKey = "loadingMessage";
+  const errorMessageKey = "errorMessage";
 
   const onPopulationByAgeChange = (newValue: string[]) => {
     console.log(newValue);
     setPopulationByAge(newValue);
   };
 
-  useLayoutEffect(() => {
-    message.loading({
-      content: "Загружаю список субъектов РФ...",
-      duration: 0,
-      key: notificationKey,
-      style: {
-        position: "absolute",
-        right: "1em",
-        top: "10vh",
-      },
-    });
-  }, []);
-
   useEffect(() => {
     async function loadSubjects() {
+      
+      // antd library doesn't supply message component with a delay
+      // so I implemented my own version
+      // the drawback is that there might be a situation where fetching subjects
+      // succeeds before the delay expires - in that case user will have to wait that additional time
+      // with that said to improve UX DO NOT SET delay over 1s
+      const loadingMessageDelay = 1000;
+      let loadingMessagePromise = new Promise(resolve => {
+        setTimeout(() => {
+          message.loading({
+            content: "Загружаю список субъектов РФ...",
+            duration: 0,
+            key: loadingMessageKey,
+            style: {
+              position: "absolute",
+              right: "1em",
+              top: "10vh",
+            },
+          });
+          resolve(undefined);
+        }, loadingMessageDelay)
+      });
+
       try {
-        let response = await fetch("http://localhost:3002/subjectTree");
-        let json = await response.json();
-        console.warn(json);
+        // extract only first promise which by the way already contains json with subjects
+        // the 2nd promise doesn't return anything useful and is only used to show loadingMessage after the delay expires
+        let [json] = await Promise.all([(await fetch("http://localhost:3002/subjectTree")).json(), loadingMessagePromise]);
         setSubjects(json);
       } catch (error) {
-        message.destroy(notificationKey);
         message.error({
           content: "Не удалось загрузить субъекты РФ",
+          key: errorMessageKey,
           style: {
             position: "absolute",
             right: "1em",
@@ -46,7 +57,7 @@ const ChartsPage = () => {
         });
         console.error("could not fetch subjects");
       } finally {
-        messageApi.destroy();
+        message.destroy(loadingMessageKey);
       }
     }
     loadSubjects();
@@ -69,7 +80,7 @@ const ChartsPage = () => {
           style={{ width: "100%" }}
           value={populationByAge}
           dropdownStyle={{ maxWidth: "100%", maxHeight: 400, overflow: "auto" }}
-          placeholder="Please select"
+          placeholder="Выберите субъекты РФ"
           allowClear
           multiple
           treeDefaultExpandAll
