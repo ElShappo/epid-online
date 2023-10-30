407974bb7bc0b70e28977dbd04ca3c94d3c90ec2
Deni Shaipov <deni.shaipp@gmail.com>
2023-10-30 15:26:53 +0300

    improved color map

--- frontend/src/pages/Charts/ChartsPage.tsx
+++ frontend/src/pages/Charts/ChartsPage.tsx
@@ -5,10 +5,34 @@ import "./ChartsPage.css";
 import MessageWrapper from "../../components/MessageWrapper";
 import { ChartDataset } from "../../types";
 
+type LineColor = {
+  backgroundColor: string;
+  borderColor: string;
+}
+
 const ChartsPage = () => {
   const [regionsIDs, setRegionsIDs] = useState<string[]>();
   const [subjects, setSubjects] = useState<any>();
   const xlabels = Array.from(Array(101).keys());
+
+  const colors: LineColor[] = [
+    {
+      backgroundColor: '#8390c7',
+      borderColor: '#8390c7'
+    },
+    {
+      backgroundColor: '#bf4b4b',
+      borderColor: '#bf4b4b'
+    },
+    {
+      backgroundColor: '#37a166',
+      borderColor: '#37a166'
+    },
+    {
+      backgroundColor: '#396d70',
+      borderColor: '#396d70'
+    },
+  ];
 
   const [totalPopulationPerAge, setTotalPopulationPerAge] = useState<ChartDataset[]>([{data: []}]);
   const [ruralToCityRatioPerAge, setRuralToCityRatioPerAge] = useState<ChartDataset[]>([{data: []}]);
@@ -26,23 +50,33 @@ const ChartsPage = () => {
       (await fetch(`http://localhost:3002/populationPerAge?key=${id}`)).json() 
     );
     let populationPerAgeData = await Promise.all(populationPerAgeJsons);
-    populationPerAgeData = populationPerAgeData.map(each => {
-      // each.backgroundColor = 'grey';
-      // each.borderColor = 'cyan';
+    populationPerAgeData = populationPerAgeData.map((each, i) => {
+      each.backgroundColor = colors[i % colors.length].backgroundColor;
+      each.borderColor = colors[i % colors.length].borderColor;
       return each;
-    })
+    });
     setTotalPopulationPerAge(populationPerAgeData);
 
     let ruralToCityRatioPerAgeJsons = (regionsIDs as string[]).map(async (id) => 
       (await fetch(`http://localhost:3002/ruralToCityRatioPerAge?key=${id}`)).json() 
     );
     let ruralToCityRatioPerAgeData = await Promise.all(ruralToCityRatioPerAgeJsons);
+    ruralToCityRatioPerAgeData = ruralToCityRatioPerAgeData.map((each, i) => {
+      each.backgroundColor = colors[i % colors.length].backgroundColor;
+      each.borderColor = colors[i % colors.length].borderColor;
+      return each;
+    });
     setRuralToCityRatioPerAge(ruralToCityRatioPerAgeData);
 
     let womenToMenRatioPerAgeJsons = (regionsIDs as string[]).map(async (id) => 
       (await fetch(`http://localhost:3002/womenToMenRatioPerAge?key=${id}`)).json() 
     );
     let womenToMenRatioPerAgeData = await Promise.all(womenToMenRatioPerAgeJsons);
+    womenToMenRatioPerAgeData = womenToMenRatioPerAgeData.map((each, i) => {
+      each.backgroundColor = colors[i % colors.length].backgroundColor;
+      each.borderColor = colors[i % colors.length].borderColor;
+      return each;
+    });
     setWomenToMenRatioPerAge(womenToMenRatioPerAgeData);
 
     // setTotalPopulationPerAge(regionsData.map(region => {
