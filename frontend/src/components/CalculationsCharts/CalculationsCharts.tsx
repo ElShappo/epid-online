e1b915120f3545e31592499d21e0dbfd5b6d9c9d
Deni Shaipov <deni.shaipp@gmail.com>
2024-02-15 14:47:38 +0300

    add skeleton for calculations related components

---
+++ frontend/src/components/CalculationsCharts/CalculationsCharts.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CalculationsCharts = () => {
+  return <div>CalculationsCharts</div>;
+};
+
+export default CalculationsCharts;
--- frontend/src/pages/Calculations/CalculationsPage.tsx
+++ frontend/src/pages/Calculations/CalculationsPage.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { TableOutlined, LineChartOutlined } from "@ant-design/icons";
+import { Tabs } from "antd";
+import CalculationsTable from "../../components/CalculationsTable/CalculationsTable";
+import CalculationsCharts from "../../components/CalculationsCharts/CalculationsCharts";
 
-const CalculationsPage = () => {
-  return <div>CalculationsPage</div>;
-};
+const items = [
+  {
+    key: "1",
+    label: "Таблица",
+    icon: <TableOutlined />,
+    children: <CalculationsTable />,
+  },
+  {
+    key: "2",
+    label: "Графики",
+    children: <CalculationsCharts />,
+    icon: <LineChartOutlined />,
+  },
+];
 
-export default CalculationsPage;
+const PopulationPage: React.FC = () => (
+  <Tabs centered size="large" defaultActiveKey="1" items={items} />
+);
+
+export default PopulationPage;
