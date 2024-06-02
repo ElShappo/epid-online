aa7ce8556bfff4cb02c871755d0215297ec1ebb6
Deni Shaipov <deni.shaipp@gmail.com>
2024-06-02 21:08:58 +0300

    add filtering to programs

--- frontend/src/pages/Programs/ProgramsPage.tsx
+++ frontend/src/pages/Programs/ProgramsPage.tsx
@@ -1,5 +1,7 @@
+import { useMemo, useState } from "react";
 import ProgramCard from "../../components/ProgramCard/ProgramCard";
 import { ProgramDetails } from "../../types";
+import { Input } from "antd";
 
 const programs: ProgramDetails[] = [
   {
@@ -10,7 +12,7 @@ const programs: ProgramDetails[] = [
     url: "/programs/population",
   },
   {
-    name: "Раcчет эпидемиологических показателей",
+    name: "Расчет эпидемиологических показателей",
     description:
       "Эта программа позволяет Вам рассчитывать ряд эпидемиологических показателей, среди которых: интенсивная заболеваемость, контактное число, риск инфицирования и т.д.",
     icon: "./disease.jpg",
@@ -31,11 +33,25 @@ const programs: ProgramDetails[] = [
 ];
 
 const ProgramsPage = () => {
+  const [filter, setFilter] = useState("");
+  const filteredPrograms = useMemo(() => {
+    return programs.filter((program) => program.name.includes(filter));
+  }, [filter]);
+
   return (
     <main className="p-6">
       <h1 className="text-center text-2xl font-medium">Список доступных программ</h1>
+      <label className="flex flex-col items-center">
+        <div className="text-center pt-6 pb-3">Поиск программы по названию: </div>
+        <Input
+          className="2xl:w-2/5 xl:w-1/2 lg:w-2/3 md:w-5/6 w-full"
+          placeholder="Начинайте вводить название программы..."
+          value={filter}
+          onInput={(evt) => setFilter((evt.target as HTMLInputElement).value)}
+        />
+      </label>
       <div className="flex flex-wrap justify-center gap-8 pt-6">
-        {programs.map((program) => {
+        {filteredPrograms.map((program) => {
           return <ProgramCard {...program} />;
         })}
       </div>
